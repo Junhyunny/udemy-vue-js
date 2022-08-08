dceda57aefdea52aabbd2983c952b3786d39338b
Junhyunny <kang3966@naver.com>
2022-08-08 16:04:31 +0900

    Add BaseButton Component

--- projects/section-10-prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
+++ projects/section-10-prj-cmp-01-starting-setup/src/components/learning-resources/LearningResource.vue
@@ -3,13 +3,13 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
+      <p>{{ description }}</p>
+      <nav>
+        <a :href="link">View Resource</a>
+      </nav>
     </base-card>
-    <p>{{ description }}</p>
-    <nav>
-      <a :href="link">View Resource</a>
-    </nav>
   </li>
 </template>
 
