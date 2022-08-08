58623cb3426be155f3071fef72e006b2eb7dd658
Junhyunny <kang3966@naver.com>
2022-08-08 13:34:02 +0900

    Add Course Goal

--- projects/section-09-cmp-adv-01-starting-setup/src/App.vue
+++ projects/section-09-cmp-adv-01-starting-setup/src/App.vue
@@ -7,6 +7,13 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <!-- 강의에서는 camel로 변경된다고 하지 않았지만, camel로 자동 변경된다. -->
+        <p>{{ slotProps["anotherProps"] }}</p>
+      </template>
+    </course-goals>
   </div>
 </template>
 
@@ -14,6 +21,7 @@
 import TheHeader from "./components/TheHeader.vue";
 import BadgeList from "./components/BadgeList.vue";
 import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoal.vue";
 
 export default {
   // Vue automatically parse CamelCase to kebab-case
@@ -22,6 +30,7 @@ export default {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
--- projects/section-09-cmp-adv-01-starting-setup/src/components/BadgeList.vue
+++ projects/section-09-cmp-adv-01-starting-setup/src/components/BadgeList.vue
@@ -1,10 +1,13 @@
 <template>
   <section>
     <base-card>
-      <!-- <template v-slot:header>
+      <!-- <template v-slot:header> -->
+      <!-- short hand v-slot is #(sharp, hashtag) -->
+      <template #header>
         <h2>Available Badges</h2>
-      </template> -->
-      <template v-slot:default>
+      </template>
+      <!-- <template v-slot:default> -->
+      <template #default>
         <ul>
           <li>
             <base-badge type="admin" caption="ADMIN"></base-badge>
