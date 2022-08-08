5c3966b9b0c3ab5c684e58a1daa23c886b474302
Junhyunny <kang3966@naver.com>
2022-08-08 12:57:01 +0900

    Add using default and named slot

---
+++ projects/section-09-cmp-adv-01-starting-setup/src/components/BaseCard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <!-- childElement -->
+    <header>
+      <!-- name is upto me, define in parent component using v-slot -->
+      <slot name="header">
+        <!-- not visible when slot is exists, this is fallback plan -->
+        <h2>The Default</h2>
+      </slot>
+    </header>
+    <!-- unnamed slot is default -->
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // this.$slots can know what data giving
+    console.log(this.$slots.header);
+  },
+};
+</script>
+
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+}
+</style>
