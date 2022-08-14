d687b779563855a7b2ab7ea7d7e51da651bdb82e
Junhyunny <kang3966@naver.com>
2022-08-15 02:01:15 +0900

    Add Coach Item

--- projects/section-16-main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
+++ projects/section-16-main-prj-01-starting-setup/src/pages/coaches/CoachesList.vue
@@ -5,21 +5,36 @@
       <button>Refresh</button>
       <router-link to="/register">Register as Coach</router-link>
     </div>
-    <ul v-if="!hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">
+    <ul v-if="hasCoaches">
+      <!-- <li v-for="coach in filteredCoaches" :key="coach.id">
         {{ coach.firstName }}
-      </li>
+      </li> -->
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :area="coach.areas"
+      >
+      </coach-item>
     </ul>
     <h3 v-else>No coaches found.</h3>
   </section>
 </template>
 
 <script>
+CoachItem;
+import CoachItem from '../../components/coaches/CoachItem.vue';
 import { mapGetters } from 'vuex';
 
 export default {
+  components: {
+    CoachItem,
+  },
   computed: {
-    ...mapGetters(['hasCoaches']),
+    ...mapGetters('coaches', ['hasCoaches']),
     filteredCoaches() {
       return this.$store.getters['coaches/coaches'];
     },
@@ -27,4 +42,15 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
