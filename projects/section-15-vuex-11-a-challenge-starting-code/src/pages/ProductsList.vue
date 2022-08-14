1bffd73f2f3a82e2afad154f9168a6cf952a1308
Junhyunny <kang3966@naver.com>
2022-08-14 14:54:10 +0900

    Add Cart Store

--- projects/section-15-vuex-11-a-challenge-starting-code/src/pages/ProductsList.vue
+++ projects/section-15-vuex-11-a-challenge-starting-code/src/pages/ProductsList.vue
@@ -17,8 +17,13 @@
 <script>
 import ProductItem from '../components/products/ProductItem.vue';
 
+import { mapGetters } from 'vuex';
+
 export default {
-  inject: ['products'],
+  // inject: ['products'],
+  computed: {
+    ...mapGetters(['products']),
+  },
   components: {
     ProductItem,
   },
@@ -26,10 +31,10 @@ export default {
 </script>
 
 <style scoped>
-  ul {
-    list-style: none;
-    margin: 2rem auto;
-    padding: 0;
-    max-width: 40rem;
-  }
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 40rem;
+}
 </style>
