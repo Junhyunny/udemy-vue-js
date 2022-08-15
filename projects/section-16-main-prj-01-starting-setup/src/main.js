4ef36ab5ab90b4962aa3de2f2c5aa03796982a78
Junhyunny <kang3966@naver.com>
2022-08-15 13:20:22 +0900

    Add base UI components

--- projects/section-16-main-prj-01-starting-setup/src/main.js
+++ projects/section-16-main-prj-01-starting-setup/src/main.js
@@ -3,8 +3,15 @@ import router from './router';
 import store from './store';
 
 import App from './App.vue';
+import BaseBadge from './components/ui/BaseBadge.vue';
+import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 
 const app = createApp(App);
+
+app.component('base-badge', BaseBadge);
+app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.use(router);
 app.use(store);
