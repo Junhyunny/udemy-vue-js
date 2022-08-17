f7de0210510ca4eefe8a823e1e2b9c409cb0247d
Junhyunny <kang3966@naver.com>
2022-08-17 09:04:14 +0900

    Add make custom hook by using composition api

---
+++ projects/section-20-reuse-04-composition-api-starting-project/src/components/AddUser.vue
@@ -0,0 +1,42 @@
+<template>
+  <user-alert v-if="alertIsVisible" title="Add a User?" @close="hideAlert">
+    <p>Do you want to continue with adding a user?</p>
+  </user-alert>
+  <section>
+    <h2>Add a User</h2>
+    <button @click="showAlert">Add User</button>
+  </section>
+</template>
+
+<script>
+// import { ref } from 'vue';
+import UserAlert from './UserAlert.vue';
+
+import { userAlert } from '../hooks/alert';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  setup() {
+    const data = userAlert();
+    return {
+      ...data,
+    };
+    // const alertIsVisible = ref(false);
+
+    // function showAlert() {
+    //   alertIsVisible.value = true;
+    // }
+    // function hideAlert() {
+    //   alertIsVisible.value = false;
+    // }
+
+    // return {
+    //   alertIsVisible,
+    //   showAlert,
+    //   hideAlert
+    // };
+  },
+};
+</script>
---
+++ projects/section-20-reuse-04-composition-api-starting-project/src/components/DeleteUser.vue
@@ -0,0 +1,44 @@
+<template>
+  <user-alert v-if="alertIsVisible" :title="alertTitle" @close="hideAlert">
+    <p>Do you want to continue with deleting a user?</p>
+  </user-alert>
+  <section>
+    <h2>Delete a User</h2>
+    <button @click="showAlert">Delete User</button>
+  </section>
+</template>
+
+<script>
+import UserAlert from './UserAlert.vue';
+
+import { userAlert } from '../hooks/alert';
+import { ref } from 'vue';
+
+export default {
+  components: {
+    UserAlert,
+  },
+  setup() {
+    const alertTitle = ref('Delete the User?');
+    const data = userAlert();
+    return {
+      ...data,
+      alertTitle,
+    };
+    // const alertIsVisible = ref(false);
+
+    // function showAlert() {
+    //   alertIsVisible.value = true;
+    // }
+    // function hideAlert() {
+    //   alertIsVisible.value = false;
+    // }
+
+    // return {
+    //   alertIsVisible,
+    //   showAlert,
+    //   hideAlert,
+    // };
+  },
+};
+</script>
